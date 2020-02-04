9c3c197efdbd88c3f09f47e8506c9efbe0946ecd
LeuAlmeida <leo@webid.net.br>
2020-02-04 18:53:48 -0300

    chore: Improving ESLint configs file

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,10 @@ module.exports = {
   env: {
     es6: true,
     node: true,
+    jest: true
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -15,9 +15,10 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    "prettier/prettier": "error",
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
-    "camelcase": "off",
-    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
+    camelcase: "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
   },
 };
