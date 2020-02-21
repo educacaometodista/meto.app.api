c0ce12d02ab01635c00ac19a8f43e21a0d76c5ec
LeuAlmeida <leo@webid.net.br>
2020-02-21 14:59:16 -0300

    feat(routes): update initial routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,19 @@
 import { Router } from 'express';
+// import multer from 'multer';
+// import multerConfig from './config/multer';
 
+import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+// import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+// const upload = multer(multerConfig);
 
 // Session Actions
-routes.post('/session', UserController.store);
+routes.post('/session', SessionController.store);
+
+routes.post('/users', UserController.store);
+
+// routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
