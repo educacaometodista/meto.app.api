3c40b06f6b32323535f37a4c0482cf2903fbf069
LeuAlmeida <leo@webid.net.br>
2020-02-21 12:55:58 -0300

    test(routes): update User login test

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -4,11 +4,15 @@ import app from '../../src/app';
 describe('User', () => {
   it('Should be able to register', async () => {
     const response = await request(app)
-      .post('/users')
+      .post('/session')
       .send({
         name: 'Léu Almeida',
         email: 'leo@webid.net.br',
+        cpf: 56754842059,
+        ra: 274881,
         password_hash: '123456',
+        gratuade: false,
+        phone: 11940028922,
       });
 
     expect(response.body).toHaveProperty('id');
