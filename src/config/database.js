4f6d526bff794c3fbb379acc2aa1d7ce1a9515dc
LeuAlmeida <leo@webid.net.br>
2020-02-21 14:57:36 -0300

    refactor(config): update database config

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,7 @@ require('../bootstrap');
 
 module.exports = {
   dialect: process.env.DB_DIALECT || 'postgres',
+  port: process.env.DB_PORT,
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
