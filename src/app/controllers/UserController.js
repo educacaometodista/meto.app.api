74f9bfd2a65b795aebc3ca89b79d2ec457390120
LeuAlmeida <leo@webid.net.br>
2020-02-21 15:19:10 -0300

    feat(UserController): create store method

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,10 +1,73 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const user = await User.create(req.body);
+    const schema = Yup.obect().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      cpf: Yup.number().required(),
+      password: Yup.string()
+        .min(6)
+        .required(),
+      phone: Yup.number()
+        .min(11)
+        .max(11)
+        .required(),
+    });
 
-    return res.json(user);
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { email, cpf, phone } = req.body;
+
+    const emailExists = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (emailExists) {
+      return res
+        .status(400)
+        .json({ error: 'An user with this email already exists.' });
+    }
+
+    const cpfExists = await User.findOne({
+      where: {
+        cpf,
+      },
+    });
+
+    if (cpfExists) {
+      return res
+        .status(400)
+        .json({ error: 'An user with this CPF already exists.' });
+    }
+
+    const phoneExists = await User.findOne({
+      where: {
+        phone,
+      },
+    });
+
+    if (phoneExists) {
+      return res
+        .status(400)
+        .json({ error: 'An user with this phone already exists.' });
+    }
+
+    const { id, name } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      cpf,
+      phone,
+    });
   }
 }
 
